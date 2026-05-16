e54534082c6077d958a3557c5c09e62d93caae94
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2026-05-16 22:35:20 +0530

    Ok

---
+++ core/supabase_client.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from supabase import create_client, Client
+
+@st.cache_resource
+def get_supabase() -> Client:
+    url = st.secrets["supabase"]["url"]
+    key = st.secrets["supabase"]["anon_key"]
+    return create_client(url, key)
