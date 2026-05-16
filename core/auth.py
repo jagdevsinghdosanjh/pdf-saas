486800aaed0dd45523cf87844e1a6f393febc8ba
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2026-05-16 22:34:48 +0530

    Ok

---
+++ core/auth.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from supabase import Client
+from .supabase_client import get_supabase
+
+def get_client() -> Client:
+    return get_supabase()
+
+def login_form():
+    st.subheader("Login")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        sb = get_client()
+        res = sb.auth.sign_in_with_password({"email": email, "password": password})
+        if res.user is None:
+            st.error("Invalid credentials")
+        else:
+            st.session_state["sb_session"] = res.session
+            st.experimental_rerun()
+
+def signup_form():
+    st.subheader("Sign up")
+    email = st.text_input("Email", key="signup_email")
+    password = st.text_input("Password", type="password", key="signup_password")
+    if st.button("Create account"):
+        sb = get_client()
+        res = sb.auth.sign_up({"email": email, "password": password})
+        if res.user is None:
+            st.error("Sign up failed")
+        else:
+            # create profile row
+            sb.table("profiles").insert({
+                "id": res.user.id,
+                "email": email,
+            }).execute()
+            st.success("Check your email to confirm, then log in.")
+
+def get_current_user():
+    sb_session = st.session_state.get("sb_session")
+    if not sb_session:
+        return None
+
+    sb = get_client()
+    res = sb.auth.get_user(sb_session.access_token)
+    return res.user
+
+def require_auth():
+    user = get_current_user()
+    if user is None:
+        st.info("Please log in to continue.")
+        login_form()
+        st.stop()
+    return user
